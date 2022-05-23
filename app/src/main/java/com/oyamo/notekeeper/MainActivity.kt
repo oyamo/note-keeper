32852b8ea9ccf4c65eb860e79e0f3ccce5beab71
oyamoh-brian <oyamobrian8@gmail.com>
2022-05-23 20:32:02 +0300

    initial commit

---
+++ app/src/main/java/com/oyamo/notekeeper/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.oyamo.notekeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * Android
+1.Architecture
+- Clean Arch
+2.Components
+ - Navigation
+3.Database
+- Room - Flow - Livedata
+4. Networking
+- Retrofit - GSON
+ 5. Paging
+ 6. Jetpack
+ */
